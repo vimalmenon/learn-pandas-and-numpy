3cefecfae23c9e80773f9b576a978d69e2899a1f
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-05-03 06:46:13 +0800

    update numpy

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Learn Pandas and NumPy
+
+
+Run Python numpy
+```sh
+python -m learn_pandas_and_numpy.numpy
+```
--- learn_pandas_and_numpy/numpy.py
+++ learn_pandas_and_numpy/numpy.py
@@ -1,3 +1,11 @@
 import numpy as np
 
-print(np.array([1, 2, 3]))
+
+def create_array_with_types():
+    value = np.array([1.8, 2.8, 3.8], dtype="int8")
+    print(value)
+    print(value.ndim)
+
+
+if __name__ == "__main__":
+    create_array_with_types()
